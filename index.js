559f83584bb6dd4d4ae1642251886d0be82cbb70
Javier Orellana <javisda@gmail.com>
2025-10-06 21:27:53 -0300

    Se carga el proyecto

--- index.js
+++ index.js
@@ -19,6 +19,30 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+const urlDatabase = [];
+
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  try{
+    const urlObj = new URL(originalUrl);
+    const hostname = urlObj.hostname;
+
+    dns.lookup(hostname, (err) =>{
+      if (err) {
+        return res.json({error: 'invalid url'});
+      }
+
+      const shorUrl = urlDatabase.length + 1;
+      urlDatabase.push({original_url: originalUrl, short_url: shortUrl});
+
+      res.json({original_url:originalUrl, shortUrl: shortUrl});
+    });
+  } catch (err) {
+    res.json({ error: 'invalid url'});
+  }
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
