a6e711f4f0d01e71fde6f819378554caa932e9c3
Javier Orellana <javisda@gmail.com>
2025-10-07 01:48:28 -0300

    se borra la parte de hello

--- index.js
+++ index.js
@@ -17,10 +17,6 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
 
 const urlDatabase = [];
 
